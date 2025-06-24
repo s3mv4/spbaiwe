aa2e23a7835a85418f4dea4eef9fde33695e159b
s3m <s3m@archlinux.org>
2025-06-24 19:40:08 +0200

    error handling

--- main.py
+++ main.py
@@ -9,11 +9,23 @@ episode = int(sys.argv[2])
 with open("data.json") as file:
     data = json.load(file)
 
+if season < 1 or season > len(data["seasons"]):
+    print(f"season {season} episode {episode} does not exist!")
+    sys.exit()
+
+if episode < 1 or episode > len(data["seasons"][season - 1]):
+    print(f"season {season} episode {episode} does not exist!")
+    sys.exit()
+
 title = data["seasons"][season - 1][episode - 1]["title"]
 
 print(f"Playing season {season} episode {episode}: {title}...")
 
 mediagen = data["seasons"][season - 1][episode - 1]["mediagen"] 
+
+if mediagen == "":
+    print(f"season {season} episode {episode}: {title} not available!")
+    sys.exit()
 
 mediagen_data = requests.get(mediagen).json() 
 
